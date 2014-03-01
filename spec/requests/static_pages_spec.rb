55e7760bb79c8c25aa780454783f917844076579
Ryan Fisher <rjfisher1@gmail.com>
2014-02-28 17:55:53 -0800

    Fix buggs from Chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,35 @@
 require 'spec_helper'
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject {page}
+  shared_examples_for "all static pages" do
+    it {should have_selector('h1', text: heading)}
+    it {should have_title(full_title(page_title))}
+  end
   describe "Home page" do
     before {visit root_path}
-    it {should have_content ('Sample App')}
-    it {should have_title (full_title(''))}
+    let (:heading) {'Sample App'}
+    let (:page_title) {''}
+    
+    it_should_behave_like "all static pages"
     it {should_not have_title("| Home")}
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About us')) 
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link "Home" 
+      click_link "Sign up now!" 
+      expect(page).to have_title(full_title('Sign up'))
+      click_link "sample app" 
+      expect(page).to have_title(full_title(''))
+    end
   end
   describe "Help page" do
     before {visit help_path}
-
-    it {should have_content('Help')}
+    it {should have_selector('h1', text: 'Help')}
     it {should have_title(full_title('Help'))}
   end
   describe "About page" do
