def8afdb8579087873f9508ee2efdaa9f8e4deb7
Ryan Fisher <rjfisher1@gmail.com>
2014-04-05 15:19:25 -0700

    removed offending '+' form config.assets.precompile to test in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ SampleApp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ]
+  config.assets.precompile = [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
