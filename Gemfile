c544ad7cc1031d7b194cb3876e3c939c3d53ac0d
rjfisher123 <rjfisher1@gmail.com>
2015-04-09 16:17:13 -0700

    Configured for deployment to Heroku

--- Gemfile
+++ Gemfile
@@ -46,4 +46,5 @@ group :doc do
 end
 group :production do
   gem 'rails_12factor', '0.0.2'
+  gem 'thin'
 end
--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.initialize_on_precompile = false
+    rake assets:precompile
     config.autoload_paths += %W(#{config.root}/lib)
     config.quiet_assets = false
     config.filter_parameters += [:password]
