d41923d9dd426ed57af63b15667602c6f232643d
rjfisher123 <rjfisher1@gmail.com>
2015-05-04 11:43:35 -0700

    save sendgrid implementation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,16 @@ SampleApp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => "intense-oasis-8834.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "example.com" }
+  ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
+  :domain               => "heroku.com",
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
